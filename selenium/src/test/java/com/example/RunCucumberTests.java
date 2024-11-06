8243a3e4338acf6b27e6a65c32ea4507dbc92b32
Wirayuda <wirayuda.senjaya@dexagroup.com>
2024-11-06 09:37:14 +0700

    update runcucumbertests

--- selenium/src/test/java/com/example/RunCucumberTests.java
+++ selenium/src/test/java/com/example/RunCucumberTests.java
@@ -10,6 +10,6 @@ import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, html:target/jsonReports/cucumber.html")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, html:target/reports/cucumber.html, json:target/reports/cucumber.json")
 public class RunCucumberTests {
 }
